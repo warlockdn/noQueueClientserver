89152a7616f805048cd043a9caf0f1b09137836b
Deepankar N <warlockdn@gmail.com>
2018-05-20 16:49:25 +0530

    Changed Authentication by Phone & Pass Added SendOTP Added Auth by OTP Changed Create Customer

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,7 @@
 const logger = require('../utils/logger');
 const customer = require('../controllers/customer');
+const sms = require('../providers/sms');
+const jwtProvider = require('../providers/token-generator');
 
 const findCustomer = async(req, res, next) => {
 
@@ -33,41 +35,161 @@ const authByPhonePass = async (req, res, next) => {
     const response = await customer.authCustomer(req, res, next);
     
     if (response === 'ERROR') {
-        res.status(401).json({
+        return res.status(401).json({
             status: 401,
             message: "Customer doesn't exist"
         });
     } else if(response === 'INCORRECT') {
-        res.status(401).json({
+        return res.status(401).json({
             status: 401,
             message: "Incorrect credentials"
         })
     } else {
-        res.status(200).json({
-            status: 200,
-            message: "Success",
+
+        const payload = {
             customer: {
                 id: response.customerID,
                 name: response.name,
                 email: response.email,
                 phone: response.phone
+            },
+            type: 'customer'
+        };
+
+        jwtProvider.generator(payload).then(
+            (token) => {
+                return res.status(200).json({
+                    status: 200,
+                    message: "Success",
+                    token: token,
+                    customer: {
+                        id: response.customerID,
+                        name: response.name,
+                        email: response.email,
+                        phone: response.phone
+                    }
+                });
             }
-        });
+        ).catch((err) => {
+            return res.status(500).json({
+                status: 500,
+                message: process.env.ERROR_MSG
+            })
+        })
+
     }
  
 };
 
-const authByOTP = async(req, res, next) => {
-
+const sendOTP = async(req, res, next) => {
+    const phone = req.body.phone;
+    logger.info(`Sending OTP to ${phone}`);
+    sms.sendOTP(phone);
+    return res.status(200).json({
+        status: 200,
+        message: `OTP send to ${phone}`
+    })
 };
 
+const authByOTP = async(req, res, next) => {
+
+    const phone = parseInt(req.body.phone);
+    const otp = req.body.otp;
+
+    try {
+        const status = await sms.verifyOTP(phone, otp);
+        console.log(status);
+        if (status.type === 'success') {
+            // Otp verified. Get customer details.
+            const response = await customer.findCustomer(phone);
+            const payload = {
+                customer: {
+                    id: response.customerID,
+                    name: response.name,
+                    email: response.email,
+                    phone: response.phone
+                },
+                type: 'customer'
+            };
+            jwtProvider.generator(payload).then((token) => {
+                res.status(200).json({
+                    status: 200,
+                    customer: {
+                        id: response.customerID,
+                        name: response.name,
+                        email: response.email,
+                        phone: response.phone
+                    },
+                    token: token
+                });
+            }).catch((err) => {
+                return res.status(500).json({
+                    status: 500,
+                    message: process.env.ERROR_MSG
+                });
+            });
+        } else {
+            throw new Error('ERROR');
+        }
+    } catch(err) {
+        logger.info(`Error verifying OTP`);
+        return res.status(500).json({
+            status: 500,
+            message: process.env.ERROR_MSG
+        });
+    }
+    
+}
+
 const createCustomer = async(req, res, next) => {
-    customer.createCustomer(req, res, next);
+    
+    try {
+        const response = await customer.createCustomer(req, res, next);
+        
+        if (!response.code) {
+            logger.info(`Account created ${response.name} - ${response.phone}`);
+        
+            const payload = {
+                customer: {
+                    id: response.customerID,
+                    name: response.name,
+                    email: response.email,
+                    phone: response.phone
+                },
+                type: 'customer'
+            };
+        
+            jwtProvider.generator(payload).then(
+                (token) => {
+                    return res.status(200).json({
+                        status: 200,
+                        message: "Success",
+                        token: token,
+                        customer: payload.customer
+                    });
+                }
+            ).catch((err) => {
+                throw new Error ('Error creating Token')
+            })
+        } else {
+            throw new Error (response);
+        }
+
+    } catch(err) {
+
+        logger.info(`Error creating customer ${err}`);
+
+        return res.status(500).json({
+            status: 500,
+            message: process.env.ERROR_MSG
+        });
+    }
 }
 
 module.exports = {
     findCustomer,
     authByPhonePass,
+    sendOTP,
     authByOTP,
     createCustomer
 }
